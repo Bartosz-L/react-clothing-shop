7bef71d431ba6938ca75aeeb83f99c0cf82cff20
Bartosz Ligęza <bligeza@tidk.pl>
2019-09-06 17:35:43 +0200

    added serviceWorker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './state/store';
-
+import * as serviceWorker from './serviceWroker';
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
@@ -14,3 +14,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
